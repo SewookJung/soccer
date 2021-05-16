e51d1c82e296402e2d89a9a5a03674e70bb60b6a
sewookJung <tpdnrqkqh@naver.com>
2021-05-16 14:42:56 +0900

    [ADD] User crud functions

--- back-end/main.py
+++ back-end/main.py
@@ -1,23 +1,46 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+
+from users.database import get_db
+from users.schemas import (
+    CreateUserRequest,
+    GetUserRequest,
+    UpdateUserRequest,
+    DeleteUserRequest,
+)
+from users.models import User
 
 app = FastAPI()
 
 
-origins = [
-    "http://localhost:3000",
-]
+@app.get("/users/{user_id}")
+async def get_user(details: GetUserRequest, db: Session = Depends(get_db)):
+    get_user = db.query(User).filter(User.id == details.id).first()
+    return get_user
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.post("/users")
+async def create_user(details: CreateUserRequest, db: Session = Depends(get_db)):
+    new_user = User(name=details.name, email=details.email, password=details.password)
+    db.add(new_user)
+    db.commit()
+    return {"success": True, "user_id": new_user.id}
 
 
-@app.get("/")
-async def home():
-    return ["test"]
+@app.delete("/users/{user_id}")
+async def delete_user(details: DeleteUserRequest, db: Session = Depends(get_db)):
+    delete_user = db.query(User).filter(User.id == details.id).first()
+    db.delete(delete_user)
+    db.commit()
+    return {"success": True}
+
+
+@app.patch("/users/{user_id}")
+async def update_user(details: UpdateUserRequest, db: Session = Depends(get_db)):
+    get_user = db.query(User).filter(User.id == details.id).first()
+
+    get_user.name = details.name
+    get_user.email = details.email
+    get_user.password = details.password
+    get_user
