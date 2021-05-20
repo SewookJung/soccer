f8a5eca407e61a9761cf433f4d4c367d2fd96fa2
sewookJung <tpdnrqkqh@naver.com>
2021-05-20 21:37:02 +0900

    [ADD] CRUD functions

--- back-end/main.py
+++ back-end/main.py
@@ -1,26 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
 from users.database import get_db
 from users.schemas import (
     CreateUserRequest,
-    GetUserRequest,
     UpdateUserRequest,
-    DeleteUserRequest,
 )
 from users.models import User
 
 app = FastAPI()
 
 
-@app.get("/users/{user_id}")
-async def get_user(details: GetUserRequest, db: Session = Depends(get_db)):
-    get_user = db.query(User).filter(User.id == details.id).first()
-    return get_user
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User is not found!")
+    return user
 
 
-@app.post("/users")
+@app.post("/users", status_code=status.HTTP_201_CREATED)
 async def create_user(details: CreateUserRequest, db: Session = Depends(get_db)):
     new_user = User(name=details.name, email=details.email, password=details.password)
     db.add(new_user)
@@ -29,8 +30,8 @@ async def create_user(details: CreateUserRequest, db: Session = Depends(get_db))
 
 
 @app.delete("/users/{user_id}")
-async def delete_user(details: DeleteUserRequest, db: Session = Depends(get_db)):
-    delete_user = db.query(User).filter(User.id == details.id).first()
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    delete_user = db.query(User).filter(User.id == user_id).first()
     db.delete(delete_user)
     db.commit()
     return {"success": True}
@@ -38,9 +39,9 @@ async def delete_user(details: DeleteUserRequest, db: Session = Depends(get_db))
 
 @app.patch("/users/{user_id}")
 async def update_user(details: UpdateUserRequest, db: Session = Depends(get_db)):
-    get_user = db.query(User).filter(User.id == details.id).first()
-
-    get_user.name = details.name
-    get_user.email = details.email
-    get_user.password = details.password
-    get_user
+    update_user = db.query(User).filter(User.id == details.id).first()
+    data = details.dict(exclude_unset=True)
+    for key, value in data.items():
+        setattr(update_user, key, value)
+    db.commit()
+    return {"success": True}
