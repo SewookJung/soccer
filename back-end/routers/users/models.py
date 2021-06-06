dc6a7eb0deb96670c3a79a0b6071a423636adf8f
sewookJung <tpdnrqkqh@naver.com>
2021-06-06 14:11:35 +0900

    [ADD] User model

---
+++ back-end/routers/users/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Boolean, String, Integer
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
