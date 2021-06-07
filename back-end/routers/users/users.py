09b77d8340b137e9c0a4438708a013f335845de4
sewookJung <tpdnrqkqh@naver.com>
2021-06-08 00:37:35 +0900

    [ADD]  Added nickname value when create user and custom exception function

--- back-end/routers/users/users.py
+++ back-end/routers/users/users.py
@@ -4,23 +4,34 @@ from fastapi.exceptions import HTTPException
 from sqlalchemy.orm.session import Session
 
 from database import get_db
+
+
 from routers.users.auth import AuthHandler
 from routers.users.models import User
 from routers.users.schemas import CreateUserRequest, AuthDetails
 
+from globals.responses import custom_exception
+
+
 auth_handler = AuthHandler()
+
 
 router = APIRouter(
     prefix="/users", tags=["users"], responses={404: {"discription": "Not Found"}}
 )
 
 
-@router.get("/{user_name}", status_code=status.HTTP_200_OK)
-async def get_user(user_name: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.name == user_name).first()
+@router.get("/{nickname}", status_code=status.HTTP_200_OK)
+async def get_user(nickname: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.nickname == nickname).first()
 
     if not user:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Not found user")
+
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Not found user",
+        )
+
     return {"success": True, "user_id": user.id}
 
 
@@ -29,18 +40,28 @@ async def get_user(user_name: str, db: Session = Depends(get_db)):
     status_code=status.HTTP_201_CREATED,
     responses={
         status.HTTP_409_CONFLICT: {
-            "description": "Conflict Email",
+            "description": "Conflict",
         }
     },
 )
 async def create_user(details: CreateUserRequest, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == details.email).first()
     if user:
+        HTTPException()
         raise HTTPException(status.HTTP_409_CONFLICT, detail="Conflict email")
+
+    user = db.query(User).filter(User.nickname == details.nickname).first()
+    if user:
+        raise HTTPException(status.HTTP_409_CONFLICT, detail="Conflict nickname")
 
     hashed_password = auth_handler.get_password_hash(details.password)
 
-    new_user = User(name=details.name, email=details.email, password=hashed_password)
+    new_user = User(
+        name=details.name,
+        email=details.email,
+        password=hashed_password,
+        nickname=details.nickname,
+    )
     db.add(new_user)
     db.commit()
 
