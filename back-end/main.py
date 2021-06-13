b3214d06cf8cbb6a6564e293bf1d0c26fd7f5fdc
sewookJung <tpdnrqkqh@naver.com>
2021-06-13 16:37:41 +0900

    [ADD] Override the Httpexception

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from routers.users import users
 
-app = FastAPI()
+
+app = FastAPI(
+    title="Soccer Project",
+    description="This is a very fancy project, with auto docs for the API and everything⚽️",
+)
+
+# Customized the Httpexception(Response json format)
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse((exc.detail), status_code=exc.status_code)
+
 
 app.include_router(users.router)
